956adec43ec651cf5e60ab200d3e84e707a3dea3
EiiiDOo <ahmed.eid159753@gmail.com>
2025-06-18 16:20:19 +0300

    add initial screens for cart, profile, categories and deliver

---
+++ lib/features/deliver/presentation/screens/deliver_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeliverScreen extends StatelessWidget {
+  const DeliverScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Deliver Screen")));
+  }
+}
