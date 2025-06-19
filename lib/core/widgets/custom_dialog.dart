aa1016dc92b374773414d0b48737b53f1ca07493
EiiiDOo <ahmed.eid159753@gmail.com>
2025-06-19 03:13:36 +0300

    edit dialog

--- lib/core/widgets/custom_dialog.dart
+++ lib/core/widgets/custom_dialog.dart
@@ -1,14 +1,26 @@
+import 'package:auvnet_internship_assessment/core/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 void customLoadingDialog(BuildContext ctx) {
   showDialog(
     context: ctx,
     barrierDismissible: false,
-    builder: (context) => AlertDialog(
-      title: Text("Wait..."),
-      content: SizedBox(
-        height: 60,
-        child: Center(child: CircularProgressIndicator()),
+    builder: (context) => Dialog(
+      backgroundColor: Colors.white,
+      elevation: 20,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(AppColors.primary),
+            ),
+            SizedBox(height: 16),
+            Text("Loading...", style: TextStyle(fontSize: 16)),
+          ],
+        ),
       ),
     ),
   );
